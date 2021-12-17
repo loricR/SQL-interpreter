4628629c1bf02cf4128a6bdf20ace1a9e45c1552
Baptiste.f <baptiste.fanjoux@utbm.fr>
2021-12-17 10:45:23 +0100

    //

--- project.c
+++ project.c
@@ -14,15 +14,18 @@ int main(int argc, char *argv[]) {
 
     // Here: check parameters with getopt
     int opt = 0;
+    char param;
     char argument[1000];
     while ((opt = getopt(argc, argv, "d:l:")) != -1){
     	switch (opt) {
     		case 'd' :
     			printf("Vous avez passe le parametre d\n");
+                param = 'd';
     			argument = optarg;
     			break;
     		case 'l' :
     			printf("Vous avez passe le parametre l\n");
+                param = 'l';
     			argument = optarg;
     			break;
     	}
